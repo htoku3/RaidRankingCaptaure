bf2919fa6ab99f18a9889b8b98545849b2c88a1f
htoku3 <86103496+htoku3@users.noreply.github.com>
2022-05-03 19:09:19 +0900

    Add files via upload
    本体をアップロード

---
+++ raid.py
@@ -0,0 +1,35 @@
+from time import sleep
+import shutil
+import glob
+import os
+
+SCREEN_SHOT_DIR = os.environ["USERPROFILE"] + "\\Documents\\"
+INIT_IDX = len(glob.glob(SCREEN_SHOT_DIR + "\capture*.png"))
+MAX_IDX = 100
+
+match = None
+# Search mst window
+while match is None:
+    for id in range(Screen.getNumberScreens()):
+        try:
+            match = Screen(id).find(Pattern("1651561551777.png").similar(0.95))
+        except FindFailed:
+            pass
+
+print(match)
+mst_app = Region(match.x - 31, match.y - 15, 1134, 638)
+
+idx = INIT_IDX
+while idx < MAX_IDX:
+    match = None
+    while match is None:
+        try:
+            match = mst_app.find(Pattern("1651552265746.png").exact())
+        except FindFailed:
+            sleep(0.2)
+    print(match)
+    sleep(0.8)
+    tmpfile =  capture(mst_app)
+    shutil.move(tmpfile, SCREEN_SHOT_DIR + "\capture_{:03}.png".format(idx))
+    sleep(4)
+    idx += 1    
