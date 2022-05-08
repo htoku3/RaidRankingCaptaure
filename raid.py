f32acc946590cf6ea955cd506d4d7c11298fe662
htoku3 <86103496+htoku3@users.noreply.github.com>
2022-05-08 17:08:34 +0900

    コンソールに付加情報を表示

--- raid.py
+++ raid.py
@@ -9,20 +9,24 @@ MAX_IDX = 999
 
 match = None
 # Search mst window
+print("Search \"BOSS\" in the screens.")
+Screen.showMonitors()
 while match is None:
     for id in range(Screen.getNumberScreens()):
         try:
             match = Screen(id).find(Pattern("1651561551777.png").similar(0.95))
+            print("Found \"BOSS\" in screen {}.".format(id))
+            print("The position of the window is set. please do not move window.")
         except FindFailed:
             sleep(0.5)
 
-print(match)
 mst_app = Region(match.x - 31, match.y - 15, 1134, 638)
 
 # Make the directory if doesn't exist.
 if not os.path.exists(SCREEN_SHOT_DIR):
     os.makedirs(SCREEN_SHOT_DIR)
 
+print("Start capturing results.")
 # Take screen shots
 idx = INIT_IDX
 while idx < MAX_IDX:
@@ -32,7 +36,7 @@ while idx < MAX_IDX:
             match = mst_app.find(Pattern("1651552265746.png").exact())
         except FindFailed:
             sleep(0.2)
-    print(match)
+    print("\"1st\" is found ({}). Capturing screen".format(match))
     sleep(0.8)
     tmpfile =  capture(mst_app)
     shutil.move(tmpfile, SCREEN_SHOT_DIR + "\capture_{:03}.png".format(idx))
